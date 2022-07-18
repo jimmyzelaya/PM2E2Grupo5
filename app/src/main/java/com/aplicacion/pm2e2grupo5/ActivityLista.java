ec25d5b2ad1b38cd2c6539477b81045bccb154d4
Joshua Mclean <86990609+JoshuaMc1@users.noreply.github.com>
2022-07-17 23:26:32 -0600

    17/07/2022
    Configuracion basica

--- app/src/main/java/com/aplicacion/pm2e2grupo5/ActivityLista.java
+++ app/src/main/java/com/aplicacion/pm2e2grupo5/ActivityLista.java
@@ -2,32 +2,83 @@ package com.aplicacion.pm2e2grupo5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.aplicacion.pm2e2grupo5.Clases.Contacts;
+import com.aplicacion.pm2e2grupo5.Configuration.Configuration;
+import com.aplicacion.pm2e2grupo5.Screen2.AdapterList;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class ActivityLista extends AppCompatActivity {
 
-    Button btnAtras;
+    Button btnLAtras, btnEliminar, btnActualizar;
+    ListView lista;
+    EditText buscar;
+    ArrayList<Contacts> contacts = new ArrayList<>();
+    RequestQueue requestQueue;
+    Contacts cont;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista);
+        init();
+    }
 
-        btnAtras = (Button) findViewById(R.id.btnAtras);
+    private void init(){
+        btnActualizar = findViewById(R.id.btnActualizar);
+        btnEliminar = findViewById(R.id.btnEliminar);
+        btnLAtras = findViewById(R.id.btnAtras);
+        lista = findViewById(R.id.txtLLista);
+        buscar = findViewById(R.id.txtBuscar);
+        requestQueue = Volley.newRequestQueue(this);
+        jsonRequest();
+        try {
+            lista.setAdapter(new AdapterList(this, contacts));
+        } catch (Exception ex) {
+            message(ex.getMessage());
+        }
+    }
 
+    private void jsonRequest(){
+        JsonArrayRequest jsonRequest = new JsonArrayRequest(Request.Method.GET, Configuration.Endpoint_get_all_contacts, null,
+                response -> {
+                    if (response.length() > 0) {
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                JSONObject objeto = response.getJSONObject(i);
+                                cont = new Contacts();
+                                cont.setId(objeto.getString("id"));
+                                cont.setNombre(objeto.getString("nombre"));
+                                cont.setTelefono(objeto.getString("telefono"));
+                                cont.setLatitud(objeto.getString("latitude"));
+                                cont.setLongitud(objeto.getString("longitude"));
+                                cont.setFirma((byte[]) objeto.get("firma"));
+                                contacts.add(cont);
+                            } catch (JSONException e) {
+                                message(e.getMessage());
+                            }
+                        }
+                    }
+                },
+                error -> message(error.getMessage())
+        );
+        requestQueue.add(jsonRequest);
+    }
 
-//VOLVER ATRAS
-        btnAtras.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(intent);
-
-            }
-        });
+    public void message(String msg){
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/aplicacion/pm2e2grupo5/Configuration/Configuration.java
@@ -0,0 +1,31 @@
+package com.aplicacion.pm2e2grupo5.Configuration;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Configuration {
+    private static final String Server_http = "http://";
+    private static final String Server_direction = ipAddres();
+    private static final String Web_api = "API-REST-Examen/";
+    private static final String Get_all_contacts = "api/read.php";
+    private static final String Get_single_contact = "api/single_read.php";
+    private static final String Create_contact = "api/create.php";
+    private static final String Update_contact = "api/update.php";
+    private static final String Delete_contact = "api/delete.php";
+
+    public static final String Endpoint_get_all_contacts = Server_http + Server_direction + Web_api + Get_all_contacts;
+    public static final String Endpoint_get_single_contact = Server_http + Server_direction + Web_api + Get_single_contact;
+    public static final String Endpoint_create_contact = Server_http + Server_direction + Web_api + Create_contact;
+    public static final String Endpoint_update_contact = Server_http + Server_direction + Web_api + Update_contact;
+    public static final String Endpoint_delete_contact = Server_http + Server_direction + Web_api + Delete_contact;
+
+    private static String ipAddres() {
+        String ip = null;
+        try {
+            ip = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return ip;
+    }
+}
